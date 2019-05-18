ff6e4dc8001acbe0871d25c66b26ab44ba88b2ea
Maaz Azmi <maaz20061992@gmail.com>
2019-05-18 15:33:24 +0530

    try catch added

--- boloindya/forum/topic/signals.py
+++ boloindya/forum/topic/signals.py
@@ -7,27 +7,41 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender=Topic)
 def save_topic(sender, instance,created, **kwargs):
-	if created:
-		all_follower_list = Follower.objects.filter(user_following = instance.user).values_list('user_follower_id',flat=True)
-		for each in all_follower_list:
-			notify = Notification.objects.create(for_user_id = each,topic = instance,notification_type='1',user = instance.user)
+	try:
+		if created:
+			all_follower_list = Follower.objects.filter(user_following = instance.user).values_list('user_follower_id',flat=True)
+			for each in all_follower_list:
+				notify = Notification.objects.create(for_user_id = each,topic = instance,notification_type='1',user = instance.user)
+	except:
+		pass
 
 @receiver(post_save, sender=Comment)
 def save_comment(sender, instance,created, **kwargs):
-	if created:
-		all_follower_list = Follower.objects.filter(user_following = instance.user).values_list('user_follower_id',flat=True)
-		for each in all_follower_list:
-			if not str(each) == str(instance.topic.user.id):
-				notify = Notification.objects.create(for_user_id = each,topic = instance,notification_type='2')
-		notify_owner = Notification.objects.create(for_user_id = instance.topic.user ,topic = instance,notification_type='3',user = instance.user)
+	try:
+		if created:
+			all_follower_list = Follower.objects.filter(user_following = instance.user).values_list('user_follower_id',flat=True)
+			for each in all_follower_list:
+				if not str(each) == str(instance.topic.user.id):
+					notify = Notification.objects.create(for_user_id = each,topic = instance,notification_type='2')
+			notify_owner = Notification.objects.create(for_user_id = instance.topic.user ,topic = instance,notification_type='3',user = instance.user)
+	except:
+		pass
+
 
 @receiver(post_save, sender=Follower)
 def save_follow(sender, instance,created, **kwargs):
-	if created:
-		notify = Notification.objects.create(for_user = instance.user_following,topic = instance,notification_type='4',user = instance.user_follower)
+	try:
+		if created:
+			notify = Notification.objects.create(for_user = instance.user_following,topic = instance,notification_type='4',user = instance.user_follower)
+	except:
+		pass
+
 
 
 @receiver(post_save, sender=Like)
 def save_follow(sender, instance,created, **kwargs):
-	if created:
-		notify = Notification.objects.create(for_user = instance.comment.user, topic = instance,notification_type='5',user = instance.user)
+	try:
+		if created:
+			notify = Notification.objects.create(for_user = instance.comment.user, topic = instance,notification_type='5',user = instance.user)
+	except:
+		pass
