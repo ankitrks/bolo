b253a67ff807a8302a585dbe8b76356d2837abea
Maaz Azmi <maaz20061992@gmail.com>
2019-05-20 18:40:16 +0530

    userprofile admin updated

--- boloindya/drf_spirit/admin.py
+++ boloindya/drf_spirit/admin.py
@@ -12,21 +12,22 @@ class UserProfileResource(resources.ModelResource):
 		model = UserProfile
 		skip_unchanged = True
 		report_skipped = True
+		list_filter = ('language', )
 		fields = ( 'user__username', 'name','language','bolo_score','user__date_joined','follow_count','follower_count','question_count','answer_count','share_count','like_count')
 
 class SingUpOTPAdmin(admin.ModelAdmin):
-    list_display = ('mobile_no', 'otp', 'is_active', 'created_at', 'used_at', 'is_reset_password', 'is_for_change_phone', 'for_user', )
-    list_editable = ('is_active', )
+	list_display = ('mobile_no', 'otp', 'is_active', 'created_at', 'used_at', 'is_reset_password', 'is_for_change_phone', 'for_user', )
+	list_editable = ('is_active', )
 admin.site.register(SingUpOTP, SingUpOTPAdmin)
 
 class WeightAdmin(admin.ModelAdmin):
-    list_display = ('features', 'weight', )
+	list_display = ('features', 'weight', )
 admin.site.register(Weight, WeightAdmin)
 
 class UserProfileAdmin(ImportExportModelAdmin):
-    list_display = ('user', 'bolo_score', )
-    list_filter = ('user__date_joined', ('user__date_joined', DateRangeFilter), )
-    resource_class = UserProfileResource
+	list_display = ('user', 'name','language','bolo_score','follow_count','follower_count','question_count','answer_count','share_count','like_count')
+	list_filter = ('user__date_joined', ('user__date_joined', DateRangeFilter), 'language')
+	resource_class = UserProfileResource
 admin.site.register(UserProfile,UserProfileAdmin)
 
 admin.site.register(AppVersion)
