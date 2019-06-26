a1522265828d60eb2b2779af0387961463fe5df2
Maaz Azmi <maaz20061992@gmail.com>
2019-06-26 12:43:46 +0530

    transcoder updated

--- boloindya/forum/topic/transcoder.py
+++ boloindya/forum/topic/transcoder.py
@@ -10,13 +10,16 @@ def transcode_media_file(input_key):
     # HLS Presets that will be used to create an adaptive bitrate playlist.
     hls_64k_audio_preset_id = '1351620000001-200071';
     hls_0400k_preset_id     = '1351620000001-200050';
-    hls_0600k_preset_id     = '1351620000001-200040';
+    # hls_0600k_preset_id     = '1351620000001-200040';
     hls_1000k_preset_id     = '1351620000001-200030';
-    hls_1500k_preset_id     = '1351620000001-200020';
+    # hls_1500k_preset_id     = '1351620000001-200020';
     hls_2000k_preset_id     = '1351620000001-200010';
 
     # HLS Segment duration that will be targeted.
-    segment_duration = '2'
+    segment_duration_audio = '10'
+    segment_duration_400 = '6'
+    segment_duration_1000 = '3'
+    segment_duration_2000 = '2'
 
     #All outputs will have this prefix prepended to their output key.
     output_key_prefix = 'elastic-transcoder/output/hls/'
@@ -33,34 +36,34 @@ def transcode_media_file(input_key):
     hls_audio = {
         'Key' : 'hlsAudio/' + output_key,
         'PresetId' : hls_64k_audio_preset_id,
-        'SegmentDuration' : segment_duration
+        'SegmentDuration' : segment_duration_audio
     }
     hls_400k = {
         'Key' : 'hls0400k/' + output_key,
         'PresetId' : hls_0400k_preset_id,
-        'SegmentDuration' : segment_duration
+        'SegmentDuration' : segment_duration_400
     }
-    hls_600k = {
-        'Key' : 'hls0600k/' + output_key,
-        'PresetId' : hls_0600k_preset_id,
-        'SegmentDuration' : segment_duration
-    }
+    # hls_600k = {
+    #     'Key' : 'hls0600k/' + output_key,
+    #     'PresetId' : hls_0600k_preset_id,
+    #     'SegmentDuration' : segment_duration
+    # }
     hls_1000k = {
         'Key' : 'hls1000k/' + output_key,
         'PresetId' : hls_1000k_preset_id,
-        'SegmentDuration' : segment_duration
+        'SegmentDuration' : segment_duration_1000
     }
-    hls_1500k = {
-        'Key' : 'hls1500k/' + output_key,
-        'PresetId' : hls_1500k_preset_id,
-        'SegmentDuration' : segment_duration
-    }
+    # hls_1500k = {
+    #     'Key' : 'hls1500k/' + output_key,
+    #     'PresetId' : hls_1500k_preset_id,
+    #     'SegmentDuration' : segment_duration
+    # }
     hls_2000k = {
         'Key' : 'hls2000k/' + output_key,
         'PresetId' : hls_2000k_preset_id,
-        'SegmentDuration' : segment_duration
+        'SegmentDuration' : segment_duration_2000
     }
-    job_outputs = [ hls_audio, hls_400k, hls_600k, hls_1000k, hls_1500k, hls_2000k ]
+    job_outputs = [ hls_audio, hls_400k, hls_1000k, hls_2000k ]
 
     playlist_name = 'hls_' + output_key
     # Setup master playlist which can be used to play using adaptive bitrate.
