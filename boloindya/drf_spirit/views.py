788080d20ec6d9dab7c13c33bcefaf510b09914a
Ubuntu <ubuntu@ip-172-31-28-180.ap-south-1.compute.internal>
2019-08-12 01:30:24 +0530

    favourites list changed to the list of users with bolo score gte 2000

--- boloindya/drf_spirit/views.py
+++ boloindya/drf_spirit/views.py
@@ -1617,11 +1617,14 @@ def follow_like_list(request):
 @api_view(['POST'])
 def get_follow_user(request):
     try:
-        all_follow_id = Follower.objects.filter(user_follower = request.user,is_active = True).values_list('user_following_id', flat=True)
-        all_vb_of_follower = Topic.objects.filter(is_vb=True,is_removed=False,user_id__in=all_follow_id).values_list('user_id',flat=True)
-        if all_vb_of_follower:
-            all_user = User.objects.filter(pk__in = all_vb_of_follower)
-            return JsonResponse({'all_follow':UserSerializer(all_user,many= True).data}, status=status.HTTP_200_OK)
+        #all_follow_id = Follower.objects.filter(user_follower = request.user,is_active = True).values_list('user_following_id', flat=True)
+        #all_vb_of_follower = Topic.objects.filter(is_vb=True,is_removed=False,user_id__in=all_follow_id).values_list('user_id',flat=True)
+        #if all_vb_of_follower:
+        #    all_user = User.objects.filter(pk__in = all_vb_of_follower)
+        #    return JsonResponse({'all_follow':UserSerializer(all_user,many= True).data}, status=status.HTTP_200_OK)
+        all_user = User.objects.filter(st__bolo_score__gte = 2000)
+        if all_user.count():
+            return JsonResponse({'all_follow':UserSerializer(all_user.order_by('?'), many= True).data}, status=status.HTTP_200_OK)
         else:
             return JsonResponse({'all_follow':[]}, status=status.HTTP_200_OK)
     except Exception as e:
