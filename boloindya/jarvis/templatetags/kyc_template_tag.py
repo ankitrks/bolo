2e2bd0b2f46c6eb52dac8275d13196dccaea2574
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2019-09-09 12:54:15 +0530

    safe url genrated

--- boloindya/jarvis/templatetags/kyc_template_tag.py
+++ boloindya/jarvis/templatetags/kyc_template_tag.py
@@ -1,22 +1,25 @@
 from django import template
-import boto3
+from boto.s3.connection import S3Connection
 from django.conf import settings
 import json
+from urlparse import urlparse
 
 register = template.Library()
 
 @register.filter(name = "get_safe_url")
 def get_safe_url(path,user):
-    filepath = path.split('https://'+settings.AWS_BUCKET_NAME+'.s3.amazonaws.com/')[0]
+    filepath = path.split('https://'+settings.AWS_BUCKET_NAME+'.s3.amazonaws.com/')[1]
+    AWS_BUCKET_NAME = urlparse(path).netloc.split('.')[0]
     print filepath,"##################"
     if user.is_authenticated() and  user.is_staff:
-        client = boto3.client('s3',aws_access_key_id = settings.AWS_ACCESS_KEY_ID,aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY)
+        s3 = S3Connection(settings.AWS_ACCESS_KEY_ID,settings.AWS_SECRET_ACCESS_KEY,is_secure=True)
+        # client = boto3.client('s3',aws_access_key_id = settings.AWS_ACCESS_KEY_ID,aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY)
         # s3 = S3Connection(settings.AWS_ACCESS_KEY_ID,
         #                     settings.AWS_SECRET_ACCESS_KEY,
         #                     is_secure=True)
         # Create a URL valid for 60 seconds.
-        return client.generate_url(60, 'GET',
-                            bucket=settings.AWS_BUCKET_NAME,
+        return s3.generate_url(60, 'GET',
+                            bucket=AWS_BUCKET_NAME,
                             key=filepath,
                             force_http=True)
 
