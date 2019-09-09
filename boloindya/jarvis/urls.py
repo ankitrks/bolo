73152930ac8278f3bb7be591d35807c4b7653c51
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2019-09-09 12:16:14 +0530

    enhancement

--- boloindya/jarvis/urls.py
+++ boloindya/jarvis/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^geturl/$',views.geturl),
     url(r'^get_kyc_user_list/$',views.get_kyc_user_list,name='get_kyc_user_list'),
     url(r'^single_kyc/$',views.get_kyc_of_user,name='get_kyc_of_user'),
-    url(r'^single_kyc/$',views.SecretFileView, name='SecretFileView'),
+    url(r'^get_secret_file/$',views.SecretFileView, name='SecretFileView'),
     url(r'^get_encashable_detail/$',views.get_encashable_detail, name='get_encashable_detail'),
     url(r'^get_single_encash_detail/$',views.get_single_encash_detail, name='get_single_encash_detail'),
     url(r'^bolo_payment/$',views.PaymentView.as_view(),name='bolo_payment'),
