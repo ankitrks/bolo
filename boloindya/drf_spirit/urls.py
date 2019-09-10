0df5b11f99ef2cf8d3da37742fd4f42d07ab3df3
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2019-09-10 11:57:48 +0530

    app version url added

--- boloindya/drf_spirit/urls.py
+++ boloindya/drf_spirit/urls.py
@@ -4,7 +4,7 @@ from .views import TopicList, TopicDetails,SearchTopic,SearchUser,replyOnTopic,c
 	verify_otp, password_set, fb_profile_settings,Usertimeline,follow_user,follow_sub_category,like,shareontimeline,GetProfile,SubCategoryList,upload_video_to_s3,comment_view,\
     follow_like_list,upload_audio_to_s3,reply_delete,editTopic,topic_delete,notification_topic,GetUserProfile,RegisterDevice,UnregisterDevice,NotificationAPI,get_bolo_score,\
     GetTopic,GetQuestion,GetAnswers,CricketMatchList,get_single_match,get_single_poll,predict,LeaderBoradList,vb_seen,VBList,ExpertList,GetHomeAnswer,transcoder_notification,\
-    vb_transcode_status,get_follow_user,upload_profile_image,get_following_list,get_follower_list,GetChallenge,GetChallengeDetails,save_android_logs,SyncDump,get_hash_list
+    vb_transcode_status,get_follow_user,upload_profile_image,get_following_list,get_follower_list,GetChallenge,GetChallengeDetails,save_android_logs,SyncDump,get_hash_list,my_app_version
 from rest_framework_simplejwt import views as jwt_views
 
 app_name = 'drf_spirit'
@@ -53,6 +53,7 @@ urlpatterns = [
     url(r'^categories/', include(category_urls)),
     url(r'^comments/', include(comment_urls)),
     url(r'^upload_profile_image$', upload_profile_image, name='upload_profile_image'),
+    url(r'^my_app_version/$', my_app_version, name='my_app_version'),
     
     url(r'^token/$', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url(r'^token/refresh/$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
