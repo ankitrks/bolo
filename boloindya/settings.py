db63c781d65cb9e13d3bd8220a3849e9c2c86ece
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2019-09-11 12:07:35 +0530

    db_changed

--- boloindya/settings.py
+++ boloindya/settings.py
@@ -241,7 +241,7 @@ HAYSTACK_CONNECTIONS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'boloindya_stage',                 # Or path to database file if using sqlite3.
+        'NAME': 'boloindya',                 # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'boloindya',
         'PASSWORD': 'bng321',
