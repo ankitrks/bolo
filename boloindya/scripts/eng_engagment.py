89dee54b95b71a8446b71bd7a70e32b721f006a0
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2019-10-17 17:15:33 +0530

    improved eng enagament

--- boloindya/scripts/eng_engagment.py
+++ boloindya/scripts/eng_engagment.py
@@ -44,23 +44,23 @@ def run():
         each_like = random.choice(list(last_n_days_post))
         if each_like.date +timedelta(minutes=10) >= now:
             number_like = random.randrange(6,100)
-        elif each_like.date +timedelta(minutes=10) <= now and each_like.date +timedelta(minutes=30) >= now and each_like.view_count > 200 and each_like.likes_count < 200:
+        elif each_like.date +timedelta(minutes=10) <= now and each_like.date +timedelta(minutes=30) >= now and each_like.view_count/10 > 200 and each_like.likes_count < 200:
             number_like = random.randrange(100,200-each_like.likes_count)
-        elif each_like.date +timedelta(minutes=30) <= now and each_like.date +timedelta(hours=2) >= now and each_like.view_count > 300 and each_like.likes_count < 300:
+        elif each_like.date +timedelta(minutes=30) <= now and each_like.date +timedelta(hours=2) >= now and each_like.view_count/10 > 300 and each_like.likes_count < 300:
             number_like = random.randrange(1,300-each_like.likes_count)
-        elif each_like.date +timedelta(hours=2) <= now and each_like.date +timedelta(hours=4) >= now and each_like.view_count > 400 and each_like.likes_count < 400:
+        elif each_like.date +timedelta(hours=2) <= now and each_like.date +timedelta(hours=4) >= now and each_like.view_count/10 > 400 and each_like.likes_count < 400:
             number_like = random.randrange(1,400-each_like.likes_count)
-        elif each_like.date +timedelta(hours=4) <= now and each_like.date +timedelta(hours=6) >= now and each_like.view_count > 500 and each_like.likes_count < 500:
+        elif each_like.date +timedelta(hours=4) <= now and each_like.date +timedelta(hours=6) >= now and each_like.view_count/10 > 500 and each_like.likes_count < 500:
             number_like = random.randrange(1,500-each_like.likes_count)
-        elif each_like.date +timedelta(hours=6) <= now and each_like.date +timedelta(hours=8) >= now and each_like.view_count > 600 and each_like.likes_count < 600:
+        elif each_like.date +timedelta(hours=6) <= now and each_like.date +timedelta(hours=8) >= now and each_like.view_count/10 > 600 and each_like.likes_count < 600:
             number_like = random.randrange(1,600-each_like.likes_count)
-        elif each_like.date +timedelta(hours=10) <= now and each_like.date +timedelta(hours=12) >= now and each_like.view_count > 700 and each_like.likes_count < 700:
+        elif each_like.date +timedelta(hours=10) <= now and each_like.date +timedelta(hours=12) >= now and each_like.view_count/10 > 700 and each_like.likes_count < 700:
             number_like = random.randrange(1,700-each_like.likes_count)
-        elif each_like.date +timedelta(hours=12) <= now and each_like.date +timedelta(hours=14) >= now and each_like.view_count > 800 and each_like.likes_count < 800:
+        elif each_like.date +timedelta(hours=12) <= now and each_like.date +timedelta(hours=14) >= now and each_like.view_count/10 > 800 and each_like.likes_count < 800:
             number_like = random.randrange(1,800-each_like.likes_count)
-        elif each_like.date +timedelta(hours=14) <= now and each_like.date +timedelta(hours=16) >= now and each_like.view_count > 900 and each_like.likes_count < 900:
+        elif each_like.date +timedelta(hours=14) <= now and each_like.date +timedelta(hours=16) >= now and each_like.view_count/10 > 900 and each_like.likes_count < 900:
             number_like = random.randrange(1,900-each_like.likes_count)
-        elif each_like.date +timedelta(hours=16) <= now and each_like.date +timedelta(hours=72) >= now and each_like.view_count > 1000 and each_like.likes_count < 1000:
+        elif each_like.date +timedelta(hours=16) <= now and each_like.date +timedelta(hours=72) >= now and each_like.view_count/10 > 1000 and each_like.likes_count < 1000:
             number_like = random.randrange(1,1000-each_like.likes_count)
         else:
             number_like = 1
