526a9bdac7c3880abfda613a4b66d10aa6a01e1e
Gitesh-Shastri <giteshshastri100@gmail.com>
2019-11-07 18:08:12 +0530

    Corrected Pagination

--- boloindya/drf_spirit/views.py
+++ boloindya/drf_spirit/views.py
@@ -2503,16 +2503,22 @@ def get_category_with_video_bytes(request):
             else:
                 all_user = User.objects.filter(st__is_popular = True)
             if all_user.count():
-                paginator.page_size = 10
-                popular_bolo = paginator.paginate_queryset(all_user, request)
-                popular_bolo = UserSerializer(popular_bolo, many=True).data
+                try:
+                    paginator.page_size = 10
+                    popular_bolo = paginator.paginate_queryset(all_user, request)
+                    popular_bolo = UserSerializer(popular_bolo, many=True).data
+                except Exception as e1:
+                    popular_bolo = []
         if request.GET.get('is_with_popular'):
             startdate = datetime.today()
             enddate = startdate - timedelta(days=15)
             topics = Topic.objects.filter(is_removed=False, is_vb=True, is_popular=True, language_id=language_id, date__gte=enddate).order_by('-view_count')
-            paginator.page_size = 10
-            topics = paginator.paginate_queryset(topics, request)
-            trending_videos = CategoryVideoByteSerializer(topics, many=True).data
+            try:
+                paginator.page_size = 10
+                topics = paginator.paginate_queryset(topics, request)
+                trending_videos = CategoryVideoByteSerializer(topics, many=True).data
+            except Exception as e1:
+                trending_videos = []
         return JsonResponse({'category_details': CategoryWithVideoSerializer(category, many=True, context={'language_id': language_id}).data, 'trending_topics': trending_videos, 'popular_boloindyans': popular_bolo, 'following_user': following_user}, status=status.HTTP_200_OK)
     except Exception as e:
         return JsonResponse({'message': 'Error Occured:'+str(e)+'',}, status=status.HTTP_400_BAD_REQUEST)
