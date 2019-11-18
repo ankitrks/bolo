99d9dfbcf3aaf33d958c116d3fda5f4cc7438177
Abhishek <babhishek@careeranna.com>
2019-11-18 15:58:51 +0530

    Fixed celery notification

--- boloindya/celery1.py
+++ boloindya/celery1.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
 
+DJANGO_SETTINGS_MODULE = 'settings'
+
 app = Celery('boloindya',
              broker='redis://localhost:6379',
              backend='redis://localhost:6379',
