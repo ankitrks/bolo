19833faa9d798e23e2aed68376ecaf1917d2523d
Gitesh-Shastri <giteshshastri100@gmail.com>
2019-11-29 17:54:55 +0530

    Notification For Popular Video Dump Test

--- boloindya/drf_spirit/serializers.py
+++ boloindya/drf_spirit/serializers.py
@@ -94,22 +94,10 @@ class CommentSerializer(ModelSerializer):
         return shortnaturaltime(instance.date)
 
 class PubSubPopularSerializer(ModelSerializer):
-    video_cdn = SerializerMethodField()
-
     class Meta:
         model = Topic
-        fields = ('id', 'm3u8_content', 'audio_m3u8_content', \
-                'video_m3u8_content', 'question_video', 'video_cdn', 'language_id')
-
-    def get_video_cdn(self,instance):
-        if instance.question_video:
-            regex= '((?:(https?|s?ftp):\\/\\/)?(?:(?:[A-Z0-9][A-Z0-9-]{0,61}[A-Z0-9]\\.)+)(com|net|org|eu))'
-            find_urls_in_string = re.compile(regex, re.IGNORECASE)
-            url = find_urls_in_string.search(instance.question_video)
-            return str(instance.question_video.replace(str(url.group()), "https://d1fa4tg1fvr6nj.cloudfront.net"))
-        else:
-            return ''
-
+        fields = ('id', 'language_id')
+        
 class TopicSerializerwithComment(ModelSerializer):
     user = SerializerMethodField()
     category = PresentableSlugRelatedField(queryset=Category.objects.all(),
--- boloindya/scripts/pubsub_popular_push.py
+++ boloindya/scripts/pubsub_popular_push.py
@@ -8,13 +8,13 @@ def run():
     pubsub_obj = Topic.objects.filter(Q(is_pubsub_popular_push=True) & Q(is_popular = False))
     if pubsub_obj:
         serialized_data = {'pupluar_data' : PubSubPopularSerializer(pubsub_obj, many=True).data}
-        devices = FCMDevice.objects.filter(user__pk=39342)
+        devices = FCMDevice.objects.filter(user__pk=41)
         print len(devices)
         # for index in xrange(0, len(devices), 1000):
         #     device = devices[index:index + 1000]
         #     print len(device)
-        # print devices
-        devices.send_message(data={'pupluar_data': serialized_data })
+        print devices
+        t = devices.send_message(data={'pupluar_data': serialized_data })
         # print devices
         # Send Popular Data to Users 
 
