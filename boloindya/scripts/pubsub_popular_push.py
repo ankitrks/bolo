cb30ea9acd7e02d3e8a4e1482c291c46d99ff9a2
Gitesh-Shastri <giteshshastri100@gmail.com>
2019-11-29 17:35:55 +0530

    Notification For Popular Video Dump Test

--- boloindya/scripts/pubsub_popular_push.py
+++ boloindya/scripts/pubsub_popular_push.py
@@ -10,11 +10,11 @@ def run():
         serialized_data = {'pupluar_data' : PubSubPopularSerializer(pubsub_obj, many=True).data}
         devices = FCMDevice.objects.filter(user__pk=39342)
         print len(devices)
-        for index in xrange(0, len(devices), 1000):
-            device = devices[index:index + 1000]
-            print len(device)
-            devices.send_message(data={'pupluar_data': serialized_data })
-        print devices
+        # for index in xrange(0, len(devices), 1000):
+        #     device = devices[index:index + 1000]
+        #     print len(device)
+        # print devices
+        devices.send_message(data={'pupluar_data': serialized_data })
         # print devices
         # Send Popular Data to Users 
 
