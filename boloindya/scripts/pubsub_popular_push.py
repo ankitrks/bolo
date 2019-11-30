47d6ff0951edb83f043425a015b0b6302fbcbc15
Gitesh-Shastri <giteshshastri100@gmail.com>
2019-11-30 17:31:10 +0530

    Changes For Script Of Popular

--- boloindya/scripts/pubsub_popular_push.py
+++ boloindya/scripts/pubsub_popular_push.py
@@ -7,14 +7,13 @@ from jarvis.models import FCMDevice
 def run():
     pubsub_obj = Topic.objects.filter(Q(is_pubsub_popular_push=True) & Q(is_popular = False))
     if pubsub_obj:
-        serialized_data = {'pupluar_data' : PubSubPopularSerializer(pubsub_obj, many=True).data}
         devices = FCMDevice.objects.filter(user__pk=41)
         print len(devices)
         # for index in xrange(0, len(devices), 1000):
         #     device = devices[index:index + 1000]
         #     print len(device)
         print devices
-        t = devices.send_message(data={'pupluar_data': serialized_data })
+        t = devices.send_message(data={'pupluar_data': 'true' })
         # print devices
         # Send Popular Data to Users 
 
