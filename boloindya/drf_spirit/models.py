7df55a3e213a6829cc51290c252beeb8a173024a
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2019-12-13 17:47:30 +0530

    from email changed

--- boloindya/drf_spirit/models.py
+++ boloindya/drf_spirit/models.py
@@ -402,7 +402,7 @@ class UserFeedback(models.Model):
             requests.post(
                 "https://api.mailgun.net/v3/mail.careeranna.com/messages",
                 auth=("api", "d6c66f5dd85b4451bbcbd94cb7406f92-bbbc8336-97426998"),
-                data={"from": "BoloIndya Feedback <feedback@mail.careeranna.com>",
+                data={"from": "BoloIndya Feedback <support@boloindya.com>",
                       "to": ["anshika@careeranna.com", "varun@careeranna.com", "maaz@careeranna.com", \
                             "ankit@careeranna.com", "bhoomika@careeranna.com"],
                       "cc":[self.contact_email],
