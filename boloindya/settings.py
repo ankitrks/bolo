84eb83864adb38466ac6cf1795532c7b1cbbe661
Akash Gautam <418akash@gmail.com>
2020-01-04 12:02:15 +0530

    code for logo detection

--- boloindya/settings.py
+++ boloindya/settings.py
@@ -15,6 +15,7 @@ import os
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV = 'boloindya'#PROJECT_PATH.split(os.sep)[-1]
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = PROJECT_PATH + '/cred.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = PROJECT_PATH + '/boloindya-vision.json'
 
 # warnings.simplefilter("default", RemovedInNextVersionWarning2)
 
