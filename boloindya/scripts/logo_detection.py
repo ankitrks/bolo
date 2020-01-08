ee77f015b0055cef5351c45b55e128ef6dad4c2b
Akash Gautam <418akash@gmail.com>
2020-01-08 13:44:16 +0530

    changes

--- boloindya/scripts/logo_detection.py
+++ boloindya/scripts/logo_detection.py
@@ -39,6 +39,7 @@ def timetostring(t):
 
 # method to identify plagarised logos in videos uploaded
 def identify_logo():
+	print("hello")
 	try:
 		client = vision.ImageAnnotatorClient()
 		today = datetime.today()
@@ -50,7 +51,7 @@ def identify_logo():
 			uri = item.backup_url
 			duration = item.media_duration 
 			time = duration.split(":")
-			#print(time)
+			print(time)
 			minute = int(time[0]) * 60
 			second = int(time[1])
 			total_second = minute + second
