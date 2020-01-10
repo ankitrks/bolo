e1118f05b1aff868561f9bdb28c9bfd7a4899636
Ubuntu <ubuntu@ip-172-31-26-178.ap-south-1.compute.internal>
2020-01-10 12:27:53 +0000

    changes

---
+++ boloindya/scripts/logo_detection.py
@@ -0,0 +1,18 @@
+import boto3
+import time
+import random
+import os
+import os, io
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from datetime import datetime,timedelta,date
+from ffmpy import FFmpeg
+from datetime import timedelta
+from google.cloud import vision
+
+def main():
+        print('main')
+
+def run():
+        print('run')
+        main()  
