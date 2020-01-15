8294a7e6fa060e088141684da9bd660488739ab0
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2020-01-15 16:22:56 +0530

    bugfix

--- boloindya/drf_spirit/serializers.py
+++ boloindya/drf_spirit/serializers.py
@@ -505,10 +505,9 @@ class CategoryWithVideoSerializer(ModelSerializer):
         if self.context.get("user_id"):
             user_id =  self.context.get("user_id")
         topics = []
+        all_seen_vb = []
         if user_id:
             all_seen_vb = VBseen.objects.filter(user_id = user_id, topic__language_id=language_id, topic__m2mcategory=instance).distinct('topic_id').values_list('topic_id',flat=True)
-        else:
-            all_seen_vb = []
         post_till = datetime.now() - timedelta(days=30)
         excluded_list =[]
         superstar_post = Topic.objects.filter(is_removed = False,is_vb = True,m2mcategory=instance,language_id = language_id,user__st__is_superstar = True, date__gte=post_till).exclude(pk__in=all_seen_vb).distinct('user_id').order_by('user_id','-date')
