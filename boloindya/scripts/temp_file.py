f18c5390193fe43f4527bef9de8a59818a0690b3
Akash Gautam <418akash@gmail.com>
2020-01-15 17:00:23 +0530

    file updated

--- boloindya/scripts/temp_file.py
+++ boloindya/scripts/temp_file.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from forum.topic.models import Topic
 from google.cloud import vision
 import io
+import boto3
+import time
+import random
+import os
+import os, io
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from datetime import datetime,timedelta,date
+from ffmpy import FFmpeg
+from datetime import timedelta
+from google.cloud import vision
+import operator
+from ffmpy import FFmpeg
+from forum.topic.models import Topic
+from forum.user.models import UserProfile
+from forum.topic.models import Notification
 client = vision.ImageAnnotatorClient()
 from ffmpy import FFmpeg
 import sys
