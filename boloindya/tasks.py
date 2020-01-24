329589c6f10a5dcb94e62fa1fa81add828f6aefd
Gitesh-Shastri <giteshshastri100@gmail.com>
2020-01-24 13:03:42 +0530

    Added Changes For Cover Pic And Notification

--- boloindya/tasks.py
+++ boloindya/tasks.py
@@ -26,11 +26,13 @@ def send_notifications_task(data, pushNotification):
         schedule_status = data.get('schedule_status', "")
         datepicker = data.get('datepicker', '')
         timepicker = data.get('timepicker', '').replace(" : ", ":")
+        image_url = data.get('image_url', '')
 
         pushNotification = PushNotification()
         pushNotification.title = upper_title
         pushNotification.description = title
         pushNotification.language = lang
+        pushNotification.image_url = image_url
         pushNotification.notification_type = notification_type
         pushNotification.user_group = user_group
         pushNotification.instance_id = id
@@ -49,9 +51,12 @@ def send_notifications_task(data, pushNotification):
             language_filter = {} 
 
             if lang != '0':
-                language_filter = { 'user__st__language': lang }
-            
-            if user_group == '1':
+                language_filter = { 'user__st__language': lang, 'is_uninstalled': False }
+
+            if data.get('category', 'Select Category') not in 'Select Category':
+                filter_list=UserProfile.objects.filter(sub_category=data.get('category', None)).values_list('user__pk', flat=True)
+                device = FCMDevice.objects.filter(user__pk__in=filter_list, user__st__language=lang)
+            elif user_group == '1':
                 end_date = datetime.today()
                 start_date = end_date - timedelta(hours=3)
                 device = FCMDevice.objects.filter(user__isnull=True, created_at__range=(start_date, end_date), is_uninstalled=False)
@@ -63,12 +68,15 @@ def send_notifications_task(data, pushNotification):
 
                 #This list contains user IDs for test users: Gitesh, Abhishek, Varun, Maaz
                 # Anshika, Bhoomika and Akash
-                filter_list = [39342, 1465, 2801, 19, 40, 328, 23, 3142, 1494]
+                filter_list = [39342, 1465, 2801, 19, 40, 328, 23, 3142, 1494, 41]
                 device = FCMDevice.objects.filter(user__pk__in=filter_list, is_uninstalled=False)
+            elif user_group == '8':
+                device = FCMDevice.objects.filter(user__pk=data.get('particular_user_id', None))
             else:
                 filter_list = []
 
-                if user_group == '3':
+
+                elif user_group == '3':
                     filter_list = VBseen.objects.distinct('user__pk').values_list('user__pk', flat=True)
                 
                 elif user_group == '4' or user_group == '5':
@@ -84,12 +92,14 @@ def send_notifications_task(data, pushNotification):
                 elif user_group == '6':
                     filter_list = Topic.objects.filter(is_vb=True).values_list('user__pk', flat=True)
 
-                device = FCMDevice.objects.exclude(user__pk__in=filter_list).filter(**language_filter, is_uninstalled=False)
+                device = FCMDevice.objects.exclude(user__pk__in=filter_list).filter(**language_filter)
 
             logger.info(device)
             device_pagination = Paginator(device, 1000)
             for index in range(1, (device_pagination.num_pages+1)):
                 device_after_slice = device_pagination.page(index)
+                for each in device_after_slice:
+                    PushNotificationUser.objects.create(user=each.user, push_notification_id=pushNotification, status='2')
                 t = device_after_slice.object_list.send_message(data={"title": title, "id": id, "title_upper": upper_title, "type": notification_type, "notification_id": pushNotification.pk})
                 logger.info(t)
     except Exception as e:
