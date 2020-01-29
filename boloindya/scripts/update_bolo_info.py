a969887d6faf68a4d50328e5986e7dea614fcc2f
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2020-01-29 14:34:18 +0530

    bolo fix calculated for each month

--- boloindya/scripts/update_bolo_info.py
+++ boloindya/scripts/update_bolo_info.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
+from forum.user.models import UserProfile
 from drf_spirit.utils import check_or_create_user_pay
 
 def run():
-    for each_user in User.objects.filter(st__is_test_user=False):
-    	check_or_create_user_pay(each_user.id)
+    for each_user in UserProfile.objects.filter(is_test_user=False).order_by('-vb_count'):
+    	check_or_create_user_pay(each_user.user.id)
