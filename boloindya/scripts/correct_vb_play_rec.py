97e1797e2ae301608bf489e6b4937a53d4e7b086
Akash Gautam <418akash@gmail.com>
2020-01-30 17:06:43 +0530

    correct vbplaytime record

--- boloindya/scripts/correct_vb_play_rec.py
+++ boloindya/scripts/correct_vb_play_rec.py
@@ -40,7 +40,7 @@ def main():
 		tot_playtime = media_details[0].media_duration 
 		tot_playtime_sec = parse_duration(tot_playtime)
 		if(curr_playtime>tot_playtime_sec):
-			print(tot_playtime_sec)
+			print(tot_playtime_sec, curr_userid, curr_videoid)
 			Topic.objects.filter(user=curr_userid, videoid=curr_videoid).update(playtime = tot_playtime_sec)
 		else:
 			print("no")	
