322ec486733beeca0f8f3de397945861dfd9fe16
Akash Gautam <418akash@gmail.com>
2020-02-03 12:14:53 +0530

    file

--- boloindya/scripts/dashboard_analytics.py
+++ boloindya/scripts/dashboard_analytics.py
@@ -40,8 +40,7 @@ def put_share_data():
 	for item in all_data:
 		if(str(item.video_type) == 'shared'):
 			curr_videoid = item.videoid
-			print(datetime.strptime(str(item.timestamp), '%Y-%m-%d') 
-			curr_date = item.timestamp 
+			curr_date = (datetime.strptime(str(item.timestamp), '%Y-%m-%d')) 
 			curr_month = curr_date.month 
 			curr_year = curr_date.year 
 			curr_day = curr_date.day
