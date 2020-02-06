d88a1e72a95e787ab459bfb5f752070805ed17b2
Akash Gautam <418akash@gmail.com>
2020-02-06 13:49:27 +0530

    file changed

--- boloindya/scripts/logo_detection.py
+++ boloindya/scripts/logo_detection.py
@@ -131,7 +131,7 @@ def identify_logo():
 					for text in texts:
 						modified_text = text.description
 						if(modified_text in plag_source):
-							f.write(iter_id + " " + str(data[0].id) + " " + video_title + " " + url_str + " " + (modified_text) + "\n")
+							f.write(str(iter_id) + " " + str(data[0].id) + " " + video_title + " " + url_str + " " + (modified_text) + "\n")
 							# Topic.objects.filter(id = iter_id).update(plag_text = str(plag_source.index(modified_text)))
 							# Topic.objects.filter(id = iter_id).update(time_deleted = datetime.now())
 							# data[0].delete()
