7d722ed6a73dc92cc658d60879fce08c2dc87058
Mohammad Maaz <mohammad.maaz.azmi@gmail.com>
2020-02-13 16:29:12 +0530

    [UNSTABLE]:in progress

--- boloindya/scripts/old_video_watermark.py
+++ boloindya/scripts/old_video_watermark.py
@@ -38,20 +38,15 @@ def run():
             cmd = ['ffmpeg','-i', each_vb.backup_url, '-vf',"[in]scale=540:-1,drawtext=text='@"+each_vb.user.username+"':x=10:y=H-th-20:fontsize=16:fontcolor=white[out]",settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename_temp]
             ps = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
             (output, stderr) = ps.communicate()
-            cmd = 'ffmpeg -i '+settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename_temp+' -ignore_loop 0 -i '+settings.PROJECT_PATH+"/boloindya/scripts/ezgif.com-resize.gif"+' -filter_complex "[1:v]format=yuva444p,setsar=1,rotate=0:c=white@0:ow=rotw(0):oh=roth(0) [rotate];[0:v][rotate] overlay=10:(main_h-overlay_h+5):shortest=1" -codec:a copy -y '+settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename
+            cmd = 'ffmpeg -i '+settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename_temp+' -ignore_loop 0 -i '+settings.PROJECT_PATH+"/boloindya/scripts/boloindya_transparent.gif"+' -filter_complex "[1:v]format=yuva444p,scale=120:120,setsar=1,rotate=0:c=white@0:ow=rotw(0):oh=roth(0) [rotate];[0:v][rotate] overlay=10:(main_h-overlay_h+5):shortest=1" -codec:a copy -y '+settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename
             subprocess.call(cmd,shell=True)
-            # ps = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-            # (output, stderr) = ps.communicate()
-            # downloaded_url = upload_media(open(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename),filename)
-            # if downloaded_url:
-            #     Topic.object.filter(pk=each_vb.id).update(downloaded_url = downloaded_url,has_downloaded_url = True)
-            # if os.path.exists(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename):
-            #     os.remove(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename_temp)
-            #     os.remove(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename)
+            downloaded_url = upload_media(open(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename),filename)
+            if downloaded_url:
+                Topic.object.filter(pk=each_vb.id).update(downloaded_url = downloaded_url,has_downloaded_url = True)
+            if os.path.exists(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename):
+                os.remove(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename_temp)
+                os.remove(settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename)
             print "bye"
             print "End time:  ",datetime.now()
         except Exception as e:
             print e
-
-
-# ['ffmpeg' ,'-i' ,each_vb.backup_url , '-ignore_loop', '0' ,'-i', "ezgif.com-optimize.gif" ,'-filter_complex', "[1:v]format=yuva444p,scale=80:80,setsar=1,rotate=PI/6:c=black@0:ow=rotw(PI/6):oh=roth(PI/6) [rotate];[0:v][rotate] overlay=20:H-h-20:shortest=1" ,'-codec:a', 'copy' ,'-y', settings.PROJECT_PATH+"/boloindya/scripts/watermark/"+filename]
