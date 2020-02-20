c0f295e94b98a4c11fe14dba3115ba3c05231b5a
Ubuntu <ubuntu@ip-172-31-28-180.ap-south-1.compute.internal>
2020-02-20 20:29:21 +0530

    playtime updator time excluded removed videos

--- boloindya/scripts/playtime_updator.py
+++ boloindya/scripts/playtime_updator.py
@@ -5,10 +5,10 @@ from django.db.models import Sum
 
 
 def run():
-    for each_vb in Topic.objects.filter(is_vb=True).order_by('-id'):
+    for each_vb in Topic.objects.exclude(is_removed = True).filter(is_vb=True).order_by('-id'):
         all_play = VideoPlaytime.objects.filter(videoid = each_vb.id)
         all_play_time = all_play.aggregate(Sum('playtime'))
-        each_vb = Topic.objects.filter(pk=each_vb.id)
+        each_vb = Topic.objects.exclude(is_removed = True).filter(pk=each_vb.id)
         if all_play_time.has_key('playtime__sum') and all_play_time['playtime__sum']:
             print 'vb_playtime:',all_play_time['playtime__sum']
             each_vb.update(vb_playtime = all_play_time['playtime__sum'])
@@ -23,12 +23,14 @@ def run():
             userprofile.update(total_time_spent = all_spent_time['total_time__sum'])
         else:
             userprofile.update(total_time_spent = 0)
-        all_topic = Topic.objects.filter(is_vb=True,user= each_user.user).order_by('-id')
+        all_topic = Topic.objects.exclude(is_removed = True).filter(is_vb=True,user= each_user.user).order_by('-id')
         all_play_time = all_topic.aggregate(Sum('vb_playtime'))
         if all_play_time.has_key('vb_playtime__sum') and all_play_time['vb_playtime__sum']:
             print 'all_video_user playtime:',all_play_time['vb_playtime__sum']
             userprofile.update(total_vb_playtime = all_play_time['vb_playtime__sum'])
         else:
             userprofile.update(total_vb_playtime = 0)
+
+
 
 
