8825f63677707a8b820375befa8d355de10d7f44
Ubuntu <ubuntu@ip-172-31-28-180.ap-south-1.compute.internal>
2020-05-13 01:33:34 +0530

    cosmetic

--- boloindya/scripts/update_tor_list.py
+++ boloindya/scripts/update_tor_list.py
@@ -9,10 +9,11 @@ def run():
     if r.status_code == 200:
         for a in r.content.split('\n'):
             if a:
-                file_str += 'deny ' + str(a) + '\n'
+                file_str += 'deny ' + str(a) + ';\n'
 
         file_str += 'deny 109.70.100.25;\ndeny 109.70.100.24;\ndeny 109.70.100.35;\ndeny 109.70.100.32;\ndeny 109.70.100.33;\ndeny 109.70.100.29;\ndeny 109.70.100.20;\n'
         f = open('/etc/nginx/tor-ip.conf', 'w')
         f.write(file_str)
         f.close()
         os.system('sudo service nginx reload')
+
