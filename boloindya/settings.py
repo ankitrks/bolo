6aed276e42f1e091186197c0a13405d70e5c0630
Khushboo Singhal <khuhiz@Khushboos-MacBook-Air.local>
2020-07-01 17:26:23 +0530

    database router added

--- boloindya/settings.py
+++ boloindya/settings.py
@@ -321,8 +321,18 @@ HAYSTACK_CONNECTIONS = {
 
 HAYSTACK_SIGNAL_PROCESSOR = 'forum.search.signals.RealtimeSignalProcessor'
 
+DATABASE_ROUTERS = ['utils.DBRouter']
 DATABASES = {
    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'boloindya',                 # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'boloindya',
+        'PASSWORD': 'bng321',
+        'HOST': 'localhost',                 # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
+    },
+    'read': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'boloindya',                 # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
