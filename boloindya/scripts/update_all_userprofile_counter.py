15643f839271c1a3644457acebe83da1ca792574
maaz <mohammad.maaz.azmi@gmail.com>
2020-07-30 22:40:03 +0530

    some changes

--- boloindya/scripts/update_all_userprofile_counter.py
+++ boloindya/scripts/update_all_userprofile_counter.py
@@ -2,6 +2,8 @@ from forum.user.utils.bolo_redis import update_userprofile_all_counter
 from forum.user.models import UserProfile
 
 def run():
-	for each_user_id in UserProfile.objects.filter(vb_count__gt=0).order_by('-is_business','-is_superstar','-is_popular','-vb_count').values_list('user_id', flat=True):
-		print each_user_id
-		update_userprofile_all_counter(each_user_id)
+    for each_user_id in UserProfile.objects.filter(vb_count__gt=0).order_by('-is_business','-is_superstar','-is_popular','-vb_count').values_list('user_id', flat=True):
+        try:
+            update_userprofile_all_counter(each_user_id)
+        except Exception as e:
+            print e
