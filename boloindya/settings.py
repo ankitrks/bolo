58da863e70e182c8dad133d055859adf8e25fad8
sarfaraz115 <sarfarazalam115@gmail.com>
2020-08-03 23:50:34 +0530

    settings and home video page change

--- boloindya/settings.py
+++ boloindya/settings.py
@@ -94,10 +94,10 @@ BASE_DIR_TRANS = os.path.dirname(os.path.dirname(__file__))
 #BASE_URL='https://www.boloindya.com/'
 #ABSOLUTE_URL='https://www.boloindya.com'
 
-BASE_URL='http://127.0.0.1:8000/'
-ABSOLUTE_URL='http://127.0.0.1:8000'
-#BASE_URL='https://stage.boloindya.com/'
-#ABSOLUTE_URL='https://stage.boloindya.com'
+# BASE_URL='http://127.0.0.1:8000/'
+# ABSOLUTE_URL='http://127.0.0.1:8000'
+BASE_URL='https://stage.boloindya.com/'
+ABSOLUTE_URL='https://stage.boloindya.com'
 #ABSOLUTE_URL='https://www.boloindya.com'
 #BASE_URL='https://www.boloindya.com/'
 #
