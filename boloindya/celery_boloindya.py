f16f5af457d575d90017acafc7009f7a2e3546eb
akashutreja <utreja.akash@gmail.com>
2020-08-24 13:35:28 +0530

    adding redis localhost for testing

--- boloindya/celery_boloindya.py
+++ boloindya/celery_boloindya.py
@@ -4,7 +4,7 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_local")
 # from django.conf import settings
-redis_host = 'boloindya-prod.quyamn.ng.0001.apne1.cache.amazonaws.com'
+redis_host = 'localhost'#'boloindya-prod.quyamn.ng.0001.apne1.cache.amazonaws.com'
 app = Celery('boloindya',
              # broker='sqs://AKIAZNK4CM5CS5K7QHHW:Y5BMVprF2FGjaEbU8HlH8AH5cbopn1SGCdlc54Wb@', # 'redis://localhost:6379',
              # broker='redis://boloindya.nf4tvd.ng.0001.aps1.cache.amazonaws.com:6379',
