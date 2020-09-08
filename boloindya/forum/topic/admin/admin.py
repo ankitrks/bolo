9c0722203da35245cd3e543c49235ef3194e71b6
ankitrks-ca <49149334+ankitrks-ca@users.noreply.github.com>
2020-09-08 15:16:05 +0530

    Removed print statement

--- boloindya/forum/topic/admin/admin.py
+++ boloindya/forum/topic/admin/admin.py
@@ -391,7 +391,6 @@ class TopicAdmin(admin.ModelAdmin): # to enable import/export, use "ImportExport
             id_list = []
             user_id_list = []
             for item in sqs:
-                print(item)
                 if not type(item.id) in (str, unicode):
                     continue
 
@@ -410,7 +409,6 @@ class TopicAdmin(admin.ModelAdmin): # to enable import/export, use "ImportExport
             paginator = self.get_paginator(request, id_list, self.list_per_page)
             page = int(request.GET.get(PAGE_VAR, 0))
             ids = paginator.page(page+1).object_list
-            print(" user ids ", user_id_list)
             queryset = queryset.filter(Q(id__in=ids) | Q(user_id__st__in=user_id_list))
 
         queryset.select_related('user', 'user__userprofile')
