d8145fdafb70ada1182f4edcb56653f48f0d2331
Ubuntu <ubuntu@ip-172-31-10-123.ap-northeast-1.compute.internal>
2020-09-19 22:06:22 +0530

    wsgi updated

--- boloindya/wsgi.py
+++ boloindya/wsgi.py
@@ -22,14 +22,14 @@ sys.path.append('/var/live_code/boloindya/boloindya')
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = ".settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boloindya.settings_local")
-os.environ.setdefault("NEW_RELIC_CONFIG_FILE", "newrelic.ini")
+os.environ.setdefault("NEW_RELIC_CONFIG_FILE", "/var/live_code/boloindya/boloindya/newrelic.ini")
 
 import os
 import socket
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 import newrelic.agent
-newrelic.agent.initialize("newrelic.ini")
+newrelic.agent.initialize("/var/live_code/boloindya/boloindya/newrelic.ini")
 application = newrelic.agent.WSGIApplicationWrapper(application)
 
 # This application object is used by any WSGI server configured to use this
