27cccf9854b93c8824241529fc489e3cb1624723
shadabmohammad1 <shadabmohammad1@gmail.com>
2020-09-24 01:07:47 +0530

    Redis cluster config added

--- boloindya/connection_utils.py
+++ boloindya/connection_utils.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.utils.functional import SimpleLazyObject
 
 import redis
+from rediscluster import RedisCluster
 # from neo4j.v1 import GraphDatabase, basic_auth
 
 # neo4j_host = settings.NEO4J_DATABASES['default']['HOST']
@@ -24,6 +25,8 @@ class ConnectionHolder:
         self._redis_logs = None
         self._redis_logs_read_only = None
 
+        self.startup_nodes = [{"host": settings.REDIS_CL_HOST, "port": settings.REDIS_CL_PORT}]
+
 
     # def _get_neo4j(self):
     #     if not self._neo4j:
@@ -36,7 +39,8 @@ class ConnectionHolder:
 
     def _get_redis(self):
         if not self._redis:
-            self._redis = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+            self._redis = RedisCluster(startup_nodes=self.startup_nodes, decode_responses=True, skip_full_coverage_check=True)
+            # self._redis = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
         return self._redis
 
     def redis(self):
@@ -44,7 +48,8 @@ class ConnectionHolder:
 
     def _get_redis_read_only(self):
         if not self._redis_read_only:
-            self._redis_read_only = redis.StrictRedis(host=settings.REDIS_RO_HOST, port=settings.REDIS_RO_PORT, db=settings.REDIS_RO_DB)
+            self._redis_read_only = RedisCluster(startup_nodes=self.startup_nodes, decode_responses=True, skip_full_coverage_check=True, readonly_mode=True)
+            # self._redis_read_only = redis.StrictRedis(host=settings.REDIS_RO_HOST, port=settings.REDIS_RO_PORT, db=settings.REDIS_RO_DB)
         return self._redis_read_only
 
     def redis_read_only(self):
