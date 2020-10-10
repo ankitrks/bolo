5aaf210594d521aef1ad5053e325c4eafa6270e2
akashutreja <utreja.akash@gmail.com>
2020-10-10 20:29:07 +0530

    changed field name

--- boloindya/booking/serializers.py
+++ boloindya/booking/serializers.py
@@ -1,7 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Booking
 
 class BookingSerializer(ModelSerializer):
-    class Meta:
-        model = Booking
-        fields = ('__all__')
+	creator_id = SerializerMethodField()
+	class Meta:
+		model = Booking
+		# fields = ('__all__')
+		exclude = ('created_at', 'last_modified', 'creator')
+
+	def get_creator_id(self, instance):
+		return instance.creator_id
